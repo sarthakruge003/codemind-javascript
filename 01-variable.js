e9387f8a6718269e154adda36d0933fb61c495ca
sarthakruge003 <sarthakruge003@gmail.com>
2025-02-12 08:37:21 +0530

    Datatypesjs
    Datatypesjs

---
+++ 01-variable.js
@@ -0,0 +1,10 @@
+var age; 
+// variable declaration
+console.log(age); //undefined
+age = "22" //Variable initialization
+console.log("AGE-" ,age);
+
+var city = "Pune";// variable declaration and initilazation in one line
+console.log("My Present City is :",city);
+
+
---
+++ 02-datatypes.js
@@ -0,0 +1,27 @@
+var age = 22;
+console.log("AGE:",age);
+var ageTypeof=typeof(age);
+console.log(ageTypeof);
+
+var weight=55;
+console.log("WEIGHT:",weight);
+var weightTypeof=typeof(weight);
+console.log(weightTypeof);
+
+var myCity="PUNE"
+console.log(myCity);
+var myCitytypeof=typeof(myCity)
+console.log(myCitytypeof);
+
+var happy=true;
+console.log(happy);
+var happytypeof=typeof(happy);
+console.log(happytypeof);
+
+console.log("======== Special Data type ========");
+var fullName;
+console.log("Before initialization fullName: ", fullName); // undefined
+var fullNameTypeOf = typeof(fullName);
+console.log("Type of fullName: ", fullNameTypeOf);
+
+
