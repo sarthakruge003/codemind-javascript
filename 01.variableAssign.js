eaddd944a85b4bd68e42ed34ffcec21a85aa0b41
sarthakruge003 <sarthakruge003@gmail.com>
2025-02-12 10:55:21 +0530

    JS Assignment01
    JS Assignment01

---
+++ 01.variableAssign.js
@@ -0,0 +1,47 @@
+//Log the variable myFullname Value before initilazation on console
+var myFullName;
+console.log(myFullName);
+
+var myFullName = "Sarthak Ruge";
+console.log("My Name is",myFullName);
+
+var myFullName = "Sarthak Prakash Ruge";
+console.log("My Full Name is",myFullName);
+
+//2
+
+var mylove="I Love only JS";
+console.log("My Love :",mylove);
+
+//3
+
+var firstName="Sarthak";
+console.log("First Name:",firstName);
+
+var lastName="Ruge";
+console.log("Last Name:",lastName);
+
+var age=21;
+console.log("Age:",age);
+
+var address="A/P ASHTA";
+console.log("Address:",address);
+
+var pincode=416301;
+console.log("Pincode:",pincode);
+
+var gender="Male";
+console.log("Gender:",gender);
+
+var ismarried="No";
+console.log("Is Married:",ismarried);
+
+
+
+
+
+
+
+
+
+
