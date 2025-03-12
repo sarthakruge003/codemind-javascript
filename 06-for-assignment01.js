6dd727598624eca53754c52ddb38fd31c726cd51
sarthakruge003 <sarthakruge003@gmail.com>
2025-03-12 12:01:15 +0530

    06-forAssignment.js
    06-forAssignment.js

---
+++ 06-for-assignment01.js
@@ -0,0 +1,33 @@
+console.log(`=====Print 5 to 15 Numbers=====`);
+for (let index = 5; index <=15; index++) {
+    console.log(index);
+    
+}
+console.log(`=====50 to 40 Number In Reverse =====`);
+
+
+for (let index = 50; index >=40; index--) {
+    console.log(index);
+}
+
+console.log(`=====First 15 ODD Numbers=====`); 
+for (let index = 1; index <30 ; index = index+2) {
+    console.log(index);
+}
+
+console.log(`=====First 10 EVEN Numbers=====`); 
+for (let index = 0; index <21 ; index = index+2) {
+    console.log(index);
+}
+console.log(`=====5 Table=====`); 
+for (let index = 5; index <=50 ; index = index+5) {
+    console.log(index);
+}
+console.log(`=====10 Table=====`); 
+for (let index = 10; index <=100 ; index = index+10) {
+    console.log(index);
+}
+console.log(`=====Reverse Of 10 Table=====`); 
+for (let index = 100; index >=10 ; index = index-10) {
+    console.log(index);
+}
