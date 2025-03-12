74a332a5695b0e62d90dbf61653003d87299f736
sarthakruge003 <sarthakruge003@gmail.com>
2025-03-12 11:51:53 +0530

    03-functionAssignment.js
    03-functionAssignment.js

---
+++ 03-04functionassignment.js
@@ -0,0 +1,44 @@
+function squareOfWordLength(str) {
+    var result = str.length;
+    console.log(`The length of words is ${result}`);
+    
+    var result = str.length * str.length;
+    console.log(`The length of square is ${result}`);
+ 
+}
+squareOfWordLength("JavaScript");
+ 
+
+function squareOfWordLength(str) {
+    var result = str.length;
+    console.log(`The length of words is ${result}`);
+    
+    var result = str.length * str.length;
+    console.log(`The length of square is ${result}`);
+ 
+}
+squareOfWordLength("Google Chrome");
+ 
+function squareOfWordLength(str) {
+    var result = str.length;
+    console.log(`The length of words is ${result}`);
+    
+    var result = str.length * str.length;
+      console.log(`The length of square is ${result}`);
+      
+ 
+}
+squareOfWordLength("Web Developer Smart");
+ 
+function show() {
+    var str = "I am UI Developer";
+    var result = str.length;
+    console.log(`The length of string is ${result}`);
+
+    var resulto = result/str.split(" ").length;
+    console.log(`The string length and divide by total no. words is ${resulto}`);
+    
+    
+    
+}
+show();
