88f20d5b1985acbccce2b49c91597743305c61b1
sarthakruge003 <sarthakruge003@gmail.com>
2025-03-12 12:14:22 +0530

    18-objectDestructing.js
    18-objectDestructing.js

---
+++ 18-object-destructing.js
@@ -0,0 +1,17 @@
+const student = {
+    name: "Jenny",
+    city: "Pune",
+    age: 24,
+    pin: 411057,
+    country: "India",
+    isMarried: true
+}
+
+let { name, city, age, country="USA", isMarried, marks=0} = student;
+console.log(name, city,age,country, isMarried);
+console.log(marks);
+
+const array = ["Jenny", "Elon", "Stew"];
+const [ n1, n2, n3="MyLove", n4=1] = array;
+console.log(n1,n2,n3,n4);
+console.log(array);
