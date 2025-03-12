b225940f3d2538ffb2a5a4545646404190e4e1cc
sarthakruge003 <sarthakruge003@gmail.com>
2025-03-12 11:58:48 +0530

    05-operator.js
    05-operator.js

---
+++ 05-operator.js
@@ -0,0 +1,16 @@
+var graeterNumber = function(n1,n2 ) {
+    var result = n1 > n2 ? n1 : n2;
+    console.log(result);
+    
+}
+graeterNumber(10,-10);
+graeterNumber(800,899);
+
+var evenOdd = function(num) {
+    var result = (num%2 == 0) ? true : false;
+    return result;
+    
+}
+var returnvalue = evenOdd(29);
+console.log(returnvalue);
+
