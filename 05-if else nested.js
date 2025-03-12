e71ab491abe6c711215d90c65c226b5623ae9203
sarthakruge003 <sarthakruge003@gmail.com>
2025-03-12 11:57:39 +0530

    05-ifElseNested.js
    05-ifElseNested.js

---
+++ 05-if else nested.js
@@ -0,0 +1,20 @@
+function dreamjob(isGotPlacement) {
+    if (isGotPlacement) {
+        console.log("ThankYou");
+        var isMnc = true;
+        if (isMnc) {
+            console.log("NICE MNC");
+            
+        } else {
+            console.log("TRY FOR MNC");
+            
+        }
+        
+    } else {
+        console.log("restart");
+        
+    }
+}
+dreamjob(true);
+
+
