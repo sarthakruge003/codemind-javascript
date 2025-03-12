943925ac052a0989c8949d4179facc7f769b2e07
sarthakruge003 <sarthakruge003@gmail.com>
2025-03-12 11:56:45 +0530

    04-operator.js
    04-operator.js

---
+++ 04-operator.js
@@ -0,0 +1,22 @@
+function isEven(num) {
+    
+    var result = num%2==0 ? "EVEN": "ODD" ;
+    console.log(result);
+    
+}
+isEven(5);
+isEven(10);
+isEven(11);
+
+console.log(`====NUMBER IS POSITIVE OR NEGATIVE====`);
+function isPositive(num) {
+    var result = num>0 ? "Positive" : "Negative";
+    console.log(result);
+    
+}
+isPositive(-1);
+
+
+
+
+
