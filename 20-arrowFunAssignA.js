cea39b7b442e042ed8ee397a2da83e81788e6ee6
sarthakruge003 <sarthakruge003@gmail.com>
2025-03-12 12:15:51 +0530

    20-arrowFunctionAsiign.js
    20-arrowFunctionAsiign.js

---
+++ 20-arrowFunAssignA.js
@@ -0,0 +1,34 @@
+let Greet =() => {
+
+    console.log("GOOD MORNING,Today is Wednesday");
+ }
+ Greet();
+
+ let mul = (n1,n2,n3=1) => {
+    console.log(`Mul is ${n1*n2*n3}`);
+    
+
+ }
+mul(5,5,2);
+mul(10,4)
+
+let para = (n1,n2,n3,n4,n5) =>{
+let result =n1 + n2 + n3 + n4 + n5;
+
+return result;
+
+ 
+
+
+}
+let result1 = para(100,100,200,349,756);
+let result = para("I am", " Learning", " ES6", " feature", " in depth")
+console.log(result1);
+console.log(result);
+
+
+
+ 
+
+ 
+
