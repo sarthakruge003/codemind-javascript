3f501f934ce08b3d8fdc0644af60761021bce665
sarthakruge003 <sarthakruge003@gmail.com>
2025-03-12 11:41:03 +0530

    array
    array

---
+++ array.js
@@ -0,0 +1,29 @@
+let arraynum = [10,20,30,40,50,80,90];
+ console.log(arraynum);
+ console.log(`Length is ${arraynum.length}`);
+
+ let arrayOne = [45,50,20,10,60,90];
+ let arrayFour = arrayOne[2];
+console.log( arrayFour);
+
+ arraynum[2]= 852;
+ console.log(arraynum);
+
+ arraynum.push(456);
+ arraynum.unshift(00);
+ console.log(arraynum);
+
+
+ let arrayk = [20,30,40,90,60];
+ console.log(arrayk);
+ 
+arrayk.pop();
+arrayk.shift();
+console.log(arrayk);
+
+let arrayo = [999,555,666,55];
+ let arrayf = arrayo.splice(2);
+ console.log(arrayf);
+ console.log(arrayo);
+ 
+ 
