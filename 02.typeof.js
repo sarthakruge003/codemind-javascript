7ba7c96d67f6cc076873c926b0a0368d1b1b3c68
sarthakruge003 <sarthakruge003@gmail.com>
2025-03-12 11:51:26 +0530

    02-typeOf.js
    02-typeOf.js

---
+++ 02.typeof.js
@@ -0,0 +1,29 @@
+
+ 
+
+
+var backname;
+console.log(`Before Initializaton bank name is : ${bankname} and Data type is:${typeof(bankname)}`);
+
+
+var bankname = "SBI BANK"
+console.log(`After Initializaton bank name is: ${bankname} and Data type is: ${typeof(bankname)}`);
+
+ 
+var marks = "90%"
+console.log(`Data type of marks is : ${typeof(marks)} and value is : ${marks}`);
+
+ 
+var isworking;
+console.log(`Before initialization : ${isworking}`);
+ 
+var isworking = true;;
+console.log(`After initialization : ${isworking} Data type is : ${typeof(isworking)}`);
+
+ var totalcount = "Hundred and Seven";
+console.log(`Total Cound is : ${totalcount} Data type is : ${typeof(totalcount)}`);
+
+var totalcount = 107;
+console.log(`Total Count is : ${totalcount} Data Type is : ${typeof(totalcount)}`);
+
+
