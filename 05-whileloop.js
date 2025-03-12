1c17f7c0b3a22e90221e550feff66229e21d7734
sarthakruge003 <sarthakruge003@gmail.com>
2025-03-12 12:00:52 +0530

    05-whileloop.js
    05-whileloop.js

---
+++ 05-whileloop.js
@@ -0,0 +1,105 @@
+console.log(`======The Number Beetween 5 to 15======`);
+
+var i= 5;
+while (i<=50) {
+    console.log(i);
+    i = i+5;
+    
+}
+console.log(`======The Number Beetween 50 to 40======`);
+var i= 50;
+while (i>=40) {
+    console.log(i);
+    i= i-1;
+    
+}
+console.log(`======First 15 odd Number======`);
+var i= 1;
+while (i<=30) {
+    console.log(i);
+    i= i+2;
+    
+    
+}
+console.log(`======First 10 Even Number======`);
+var i= 0;
+while (i<=21) {
+    console.log(i);
+    i= i+2;
+
+    
+    
+}
+console.log(`====== Table of 5 ======`);
+var i= 5;
+while (i<=50) {
+    console.log(i);
+    i = i+5;
+    
+    
+}
+console.log(`====== Table of 10 ======`);
+var i =10;
+while (i<=100) {
+    console.log(i);
+    i = i+10;
+    
+    
+}
+console.log(`====== Reverse Table of 10 ======`);
+var i=100;
+while (i>=10) {
+    console.log(i);
+    i= i-10;
+    
+    
+}
+
+console.log(`======Do while=======`);
+console.log(`======The Number Beetween 5 to 15======`);
+var i= 5;
+do {
+    console.log(i);
+    i = i+1;
+    
+} while (i<=15);
+
+console.log(`======The Number Beetween 50 to 40======`);
+var i = 50;
+do {
+    console.log(i);
+    i = i-1;
+    
+} while (i>=40);
+console.log(`======First 15 odd Number======`);
+var i= 1;
+do {
+    console.log(i);
+    i = i+2;
+    
+    
+} while (i<=30);
+console.log(`======First 10 Even Number======`);
+var i= 0;
+do {
+    console.log(i);
+    i = i+2;
+    
+    
+} while (i<=20);
+console.log(`======Table of 10 ======`);
+var i= 10;
+do {
+    console.log(i);
+    i = i+10;
+    
+    
+} while (i<=100);
+console.log(`======Reverse Table of 10 ======`);
+var i= 100;
+do {
+    console.log(i);
+    i = i-10;
+    
+    
+} while (i>=10);
